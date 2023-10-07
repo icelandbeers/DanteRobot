f558a7ae7d5a21ff93cf1c267a1201ea1e7c979c
DANTE <104479517+xflydan@users.noreply.github.com>
2023-10-08 05:03:26 +0700

    Update config.py
    Signed-off-by: DANTE <104479517+xflydan@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -5,22 +5,22 @@ class Config(object):
     API_ID = 25378186
     API_HASH = "b8f77d40f8f7b955f5d34c3539a0e287"
 
-    CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
+    CASH_API_KEY = "0XYNIGBARN825UR4"  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://wfzwsndx:xNwxRIZIHhCJ2OtYOupsiKEEDz_TnyZP@silly.db.elephantsql.com/wfzwsndx"  # A sql database url from elephantsql.com
 
-    EVENT_LOGS = ()  # Event logs channel to note down important bot level events
+    EVENT_LOGS = (-1001870229808)  # Event logs channel to note down important bot level events
 
-    MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
+    MONGO_DB_URI = "mongodb+srv://idmn1:idmn1@cluster0.vuda5r7.mongodb.net/?retryWrites=true&w=majority"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://te.legra.ph/file/40eb1ed850cdea274693e.jpg"
 
-    SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "DanteXSupport"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "6686694921:AAHQrYTROu3dAYBZ5M7HM3aH_1vxDSb9mmY"  # Get bot token from @BotFather on Telegram
 
-    TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
+    TIME_API_KEY = "AZ8RZLD3B2TS"  # Get this value from https://timezonedb.com/api
 
     OWNER_ID = 1356469075  # User id of your telegram account (Must be integer)
 
