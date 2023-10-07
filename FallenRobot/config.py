022f418faa8b34cff511c61ff67450985f1ba15b
DANTE <104479517+xflydan@users.noreply.github.com>
2023-10-08 05:05:53 +0700

    Update config.py
    Signed-off-by: DANTE <104479517+xflydan@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -14,7 +14,7 @@ class Config(object):
     MONGO_DB_URI = "mongodb+srv://idmn1:idmn1@cluster0.vuda5r7.mongodb.net/?retryWrites=true&w=majority"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://te.legra.ph/file/40eb1ed850cdea274693e.jpg"
+    START_IMG = "https://telegra.ph/file/0503186cae8a355ef894b.jpg"
 
     SUPPORT_CHAT = "DanteXSupport"  # Your Telegram support group chat username where your users will go and bother you
 
