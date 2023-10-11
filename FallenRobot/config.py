4d132969783d60bd3356ae7e3961858c9eb6c861
DANTE <104479517+xflydan@users.noreply.github.com>
2023-10-11 20:43:47 +0700

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -7,7 +7,7 @@ class Config(object):
 
     CASH_API_KEY = "0XYNIGBARN825UR4"  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
-    DATABASE_URL = "postgres://wfzwsndx:xNwxRIZIHhCJ2OtYOupsiKEEDz_TnyZP@silly.db.elephantsql.com/wfzwsndx"  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://vupqxsna:1tWHQT_SBnT1mMMPgHlUbkpXGd9Y5rMn@mahmud.db.elephantsql.com/vupqxsna"  # A sql database url from elephantsql.com
 
     EVENT_LOGS = (-1001870229808)  # Event logs channel to note down important bot level events
 
