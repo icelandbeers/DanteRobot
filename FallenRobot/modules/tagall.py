54f787911b990de82cdca664920baa5c312bd7df
DANTE <104479517+xflydan@users.noreply.github.com>
2023-10-14 17:11:24 +0700

    Update tagall.py

--- FallenRobot/modules/tagall.py
+++ FallenRobot/modules/tagall.py
@@ -10,7 +10,7 @@ from FallenRobot import telethn as client
 spam_chats = []
 
 
-@client.on(events.NewMessage(pattern="^/tagall ?(.*)"))
+@client.on(events.NewMessage(pattern="^/all ?(.*)"))
 @client.on(events.NewMessage(pattern="^@all ?(.*)"))
 async def mentionall(event):
     chat_id = event.chat_id
