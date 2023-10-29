de060aa09a1d5715e5a5b34b2f1d14d49570d6ce
DANTE <104479517+xflydan@users.noreply.github.com>
2023-10-29 15:05:05 +0700

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -2,30 +2,30 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 25989336
-    API_HASH = "af53cce8dc3b6fb2e4ae0c17e9d646df"
+    API_ID = 27192333
+    API_HASH = "f5e17d3232f7e1ac576befaf319665b8"
 
     CASH_API_KEY = "0XYNIGBARN825UR4"  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
     DATABASE_URL = "postgres://vupqxsna:1tWHQT_SBnT1mMMPgHlUbkpXGd9Y5rMn@mahmud.db.elephantsql.com/vupqxsna"  # A sql database url from elephantsql.com
 
-    EVENT_LOGS = (-1001534990953)  # Event logs channel to note down important bot level events
+    EVENT_LOGS = (-1001919016639)  # Event logs channel to note down important bot level events
 
     MONGO_DB_URI = "mongodb+srv://idmn1:idmn1@cluster0.vuda5r7.mongodb.net/?retryWrites=true&w=majority"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://telegra.ph/file/0503186cae8a355ef894b.jpg"
 
-    SUPPORT_CHAT = "DanteXSupport"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "DanteSupportss"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = "6773506341:AAE7gssivqWO8Z-j1OBq4CuNIDERWrulEHo"  # Get bot token from @BotFather on Telegram
+    TOKEN = "6975735824:AAGTZjrXMpR6KSbXWxXg7V8w46w-mBQnYLU"  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = "AZ8RZLD3B2TS"  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 6569204839  # User id of your telegram account (Must be integer)
+    OWNER_ID = 2025158952  # User id of your telegram account (Must be integer)
 
     # Optional fields
-    BL_CHATS = [-1001870229808]  # List of groups that you want blacklisted.
+    BL_CHATS = []  # List of groups that you want blacklisted.
     DRAGONS = [2025158952]  # User id of sudo users
     DEV_USERS = [2025158952]  # User id of dev users
     DEMONS = [2025158952]  # User id of support users
